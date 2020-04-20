d00bbacd75564cbcc7c428c7d9416c0ef04950da
kuldeepbamrah <boby.sbamrah@gmail.com>
2020-04-20 02:34:30 -0400

    Remember Me Functionality

--- app/src/main/java/com/example/serwe/SignIn.java
+++ app/src/main/java/com/example/serwe/SignIn.java
@@ -1,7 +1,9 @@
 package com.example.serwe;
 
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +27,9 @@ public class SignIn extends AppCompatActivity {
     EditText edtPhone,edtPassword;
     Button btnLogin;
     CheckBox rememberMe;
+    String uname,pass;
+    private static final String SHARED_PREFS = "sharedPrefs";
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +40,19 @@ public class SignIn extends AppCompatActivity {
         edtPhone = (EditText)findViewById(R.id.edtPhone);
         btnLogin =  (Button) findViewById(R.id.btnLogin);
         rememberMe = findViewById(R.id.rememberMeBtn);
+
+        sharedPreferences =this.getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+       if(sharedPreferences.contains("uname"));
+        {
+            Toast.makeText(this,"exiists",Toast.LENGTH_SHORT).show();
+           uname= loadData(this,"uname");
+           pass = loadData(this,"pass");
+           edtPhone.setText(uname);
+           edtPassword.setText(pass);
+           rememberMe.setActivated(true);
+        }
+
+
 
 
         //Init Firebase
@@ -63,6 +81,14 @@ public class SignIn extends AppCompatActivity {
                                 if (user.getPassword().equals(edtPassword.getText().toString())) {
 
 
+                                    if(rememberMe.isChecked())
+                                    saveData(getApplicationContext(),edtPhone.getText().toString(),edtPassword.getText().toString());
+                                    if(!rememberMe.isChecked())
+                                        if(sharedPreferences.contains("uname")) {
+                                            sharedPreferences.edit().clear().apply();
+                                            Toast.makeText(getApplicationContext(),"deleted",Toast.LENGTH_SHORT).show();
+                                        }
+
                                     Intent homeIntent= new Intent(SignIn.this,Home.class);
                                     Common.currentUser=user;
                                     startActivity(homeIntent);
@@ -86,5 +112,19 @@ public class SignIn extends AppCompatActivity {
                     });
             }
         });
+    }
+
+    public static void saveData(Context context,String uname,String pass) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("pass", pass);
+        editor.putString("uname",uname);
+        editor.apply();
+    }
+
+    public static String loadData(Context context,String s) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String pass = sharedPreferences.getString(s, "");
+        return pass;
     }
 }
